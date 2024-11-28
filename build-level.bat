d338eb27ffb98d2b6f61e647ede4366e4df31a0d
parchii <wainggan@gmail.com>
2024-11-27 23:56:50 -0800

    idk

--- build-level.bat
+++ build-level.bat
@@ -1,4 +1,4 @@
 cd .\tools\plevel
 set RUST_BACKTRACE=1
-cargo run --release ..\..\map\level.ldtk ..\..\src\datafiles\world.bin
+cargo run ..\..\map\level.ldtk ..\..\src\datafiles\world.bin
 cd ..\..
