2cc957b6bc8ab2ee018feca1366f1a7a3a305162
parchii <wainggan@gmail.com>
2024-12-09 23:54:09 -0800

    read file

--- tools/myano/src/main.rs
+++ tools/myano/src/main.rs
@@ -10,7 +10,12 @@ mod test;
 fn main() {
 	let mut reporter = error::Reporter::new();
 
-	let tokens = token::tokenize(&mut reporter, "let a");
+	let file = match std::fs::read_to_string("input.myano") {
+		Ok(v) => v,
+		Err(e) => panic!("{}", e),
+	};
+
+	let tokens = token::tokenize(&mut reporter, &file);
 	println!("{:?}", tokens);
 
 	let ast = parse::parse(&mut reporter, &tokens);
