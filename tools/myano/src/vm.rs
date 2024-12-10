d01072165a6824bead5442a8c1b195de99cfca60
parchii <wainggan@gmail.com>
2024-12-10 03:05:37 -0800

    stuff

--- tools/myano/src/vm.rs
+++ tools/myano/src/vm.rs
@@ -1,13 +1,12 @@
 
+use std::io::Read;
+
 use crate::expr::{self, Visitor};
 
-enum OP {
-	Lit = 0x00,
-
-	Add = 0x10,
-	Sub = 0x11,
-
-	Nop = 0xff,
+mod op {
+	pub const NOP: u8 = 0xff;
+	pub const ADD: u8 = 0x10;
+	pub const LIT: u8 = 0x00;
 }
 
 
@@ -20,10 +19,10 @@ impl Compiler {
 			data: Vec::new(),
 		}
 	}
-	fn compile(&mut self, node: &expr::Node) -> &Vec<u8> {
+	fn compile(&mut self, node: &expr::Node) -> Vec<u8> {
 		self.data.clear();
 		self.resolve(node);
-		&self.data
+		self.data.clone()
 	}
 }
 impl Visitor for Compiler {
@@ -46,7 +45,7 @@ impl Visitor for Compiler {
 	fn visit_binary(&mut self, node: &expr::Binary) -> Self::Result {
 		self.resolve(&node.left);
 		self.resolve(&node.right);
-		self.data.push(OP::Add as u8);
+		self.data.push(op::ADD as u8);
 	}
 
 	fn visit_unary(&mut self, node: &expr::Unary) -> Self::Result {
@@ -62,29 +61,67 @@ impl Visitor for Compiler {
 	}
 
 	fn visit_lit_int(&mut self, node: &expr::LitInt) -> Self::Result {
-		self.data.push(OP::Lit as u8);
+		self.data.push(op::LIT as u8);
 		self.data.extend_from_slice(&(node.value as u32).to_le_bytes());
 	}
 
 	fn visit_lit_flt(&mut self, node: &expr::LitFlt) -> Self::Result {
-		self.data.push(OP::Lit as u8);
+		self.data.push(op::LIT as u8);
 		self.data.extend_from_slice(&(node.value as u32).to_le_bytes());
 	}
 }
 
 
 struct VM {
-	pc: u32,
+	bin: Vec<u8>,
+	pc: usize,
+	stack: Vec<u32>,
 }
 impl VM {
-	fn new() -> VM {
+	fn new(bin: Vec<u8>) -> VM {
 		VM {
+			bin,
+			stack: Vec::new(),
 			pc: 0,
 		}
 	}
 
-	fn run(&mut self, bin: &Vec<u8>) {
-		
+	fn step(&mut self) -> bool {
+		if self.pc >= self.bin.len() {
+			return false;
+		}
+
+		let op = self.bin[self.pc];
+		match op {
+			op::LIT => {
+				self.pc += 1;
+				let value = u32::from_le_bytes(
+					self.bin[self.pc..self.pc + 4]
+						.try_into()
+						.unwrap_or([0, 0, 0, 0])
+				);
+				self.stack.push(value);
+			},
+			op::ADD => {
+				let b = self.stack.pop().unwrap();
+				let a = self.stack.pop().unwrap();
+				self.stack.push(a + b);
+			},
+			op::NOP => {
+				panic!("lmfao");
+			},
+			_ => {
+				panic!("skill issue");
+			},
+		}
+
+		return true;
+	}
+
+	fn run(&mut self) {
+		while self.step() {
+			println!("{} {:?}", self.pc, self.stack);
+		}
 	}
 }
 
